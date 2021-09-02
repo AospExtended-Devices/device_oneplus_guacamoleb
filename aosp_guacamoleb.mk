61bf87d110850f7d7f9c00b1b324ab9c05c4219d
Shubham <skpawar1305@gmail.com>
2021-09-02 13:33:55 +0000

    Xtended -> AEX

--- aosp_guacamoleb.mk
+++ aosp_guacamoleb.mk
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from guacamoleb device
 $(call inherit-product, device/oneplus/guacamoleb/device.mk)
 
-# Inherit some common Xtended stuff.
-$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+# Inherit some common AEX stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := xtended_guacamoleb
+PRODUCT_NAME := aosp_guacamoleb
 PRODUCT_DEVICE := guacamoleb
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := OnePlus 7
@@ -37,8 +37,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="OnePlus7-user 11 RKQ1.201022.002 2106290956 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
-
-# Xtended flags
-XTENDED_BUILD_TYPE := OFFICIAL
-XTENDED_BUILD_MAINTAINER := skpawar1305
-XTENDED_BUILD_DONATE_URL := www.paypal.com/paypalme/SPawar670
